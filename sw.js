07796fdcbbe297f8df04a870ccdc68ce6aa26c71
acacioaf <114199499+acacioaf@users.noreply.github.com>
2026-02-25 00:23:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'escalas-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html', './style.css', './script.js']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
